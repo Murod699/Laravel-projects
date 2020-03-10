079fc9aa4ca0cdd269e9bf73ea8f8f922a624670
Murodjon <murodbek.0338@gmail.com>
2020-03-10 10:58:36 +0500

    public put on bootstrap

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,20 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Route::get('/', function(){
+//     return view('welcome');
+// });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Home page
+Route::get('/', 'SiteController@home')->name('home');
+// //Services page
+Route::get('/services', 'SiteController@services')->name('services');
+// //News
+Route::get('/news','SiteController@news')->name('news');
+Route::get('/news/{id}','SiteController@newsMore')->name('news.more');
+// //About us
+Route::get('/about-us', function(){
+    return "News";
+})->name('about');
+// //Contact
+Route::get('/feedback', 'SiteController@contact')->name('contact');
