7f521c151bdd7bd4fa7539c55ad2b4b5fd7506da
Murodjon <murodbek.0338@gmail.com>
2020-03-20 13:24:53 +0500

    yangilandi

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -9,7 +9,7 @@
                         
             <div class="row justify-content-center mb-4">
               <div class="col-md-8 text-center">
-                <h1>Our Blog</h1>
+                <h1>Yangiliklar </h1>
                 <p class="lead mb-5">We Make Beautiful Things</p>
               </div>
             </div>
@@ -22,71 +22,27 @@
     <section class="site-section">
       <div class="container">
         <div class="row">
-
+          @foreach($posts as $post)
           <div class="col-md-8">
             <div class="row mb-5">
               <div class="col-md-6 col-lg-6 mb-4 mb-lg-4">
                 <div class="h-entry">
                   <img src="images/img_6.jpg" alt="Image" class="img-fluid">
-                  <h2 class="font-size-regular"><a href="#">Warehousing Your Packages</a></h2>
-                  <div class="meta mb-4">Theresa Winston <span class="mx-2">&bullet;</span> Jan 18, 2019<span class="mx-2">&bullet;</span> <a href="#">News</a></div>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
+                  <a href="{{route('news.more', '$post->$id') }}">
+                    <h2 class="font-size-regular">{{$post->title}}</h2>
+                  </a>
+                  <div class="meta mb-4">
+                    <span class="mx-2">&bullet;</span>
+                    {{$post->created_at->format('d/m/y')}}
+                    <span class="mx-2">&bullet;</span>
+                    <a href="#">News</a>
+                  </div>
+                  <p>{{$post->short}}</p>
                 </div> 
-              </div>
-              <div class="col-md-6 col-lg-6 mb-4 mb-lg-4">
-                <div class="h-entry">
-                  <img src="images/img_1.jpg" alt="Image" class="img-fluid">
-                  <h2 class="font-size-regular"><a href="#">Warehousing Your Packages</a></h2>
-                  <div class="meta mb-4">Theresa Winston <span class="mx-2">&bullet;</span> Jan 18, 2019<span class="mx-2">&bullet;</span> <a href="#">News</a></div>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
-                </div>
-              </div>
-              <div class="col-md-6 col-lg-6 mb-4 mb-lg-4">
-                <div class="h-entry">
-                  <img src="images/img_3.jpg" alt="Image" class="img-fluid">
-                  <h2 class="font-size-regular"><a href="#">Warehousing Your Packages</a></h2>
-                  <div class="meta mb-4">Theresa Winston <span class="mx-2">&bullet;</span> Jan 18, 2019<span class="mx-2">&bullet;</span> <a href="#">News</a></div>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
-                </div> 
-              </div>
-              <div class="col-md-6 col-lg-6 mb-4 mb-lg-4">
-                <div class="h-entry">
-                  <img src="images/img_2.jpg" alt="Image" class="img-fluid">
-                  <h2 class="font-size-regular"><a href="#">Warehousing Your Packages</a></h2>
-                  <div class="meta mb-4">Theresa Winston <span class="mx-2">&bullet;</span> Jan 18, 2019<span class="mx-2">&bullet;</span> <a href="#">News</a></div>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
-                </div>
-              </div>
-              <div class="col-md-6 col-lg-6 mb-4 mb-lg-4">
-                <div class="h-entry">
-                  <img src="images/img_5.jpg" alt="Image" class="img-fluid">
-                  <h2 class="font-size-regular"><a href="#">Warehousing Your Packages</a></h2>
-                  <div class="meta mb-4">Theresa Winston <span class="mx-2">&bullet;</span> Jan 18, 2019<span class="mx-2">&bullet;</span> <a href="#">News</a></div>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
-                </div> 
-              </div>
-              <div class="col-md-6 col-lg-6 mb-4 mb-lg-4">
-                <div class="h-entry">
-                  <img src="images/img_1.jpg" alt="Image" class="img-fluid">
-                  <h2 class="font-size-regular"><a href="#">Warehousing Your Packages</a></h2>
-                  <div class="meta mb-4">Theresa Winston <span class="mx-2">&bullet;</span> Jan 18, 2019<span class="mx-2">&bullet;</span> <a href="#">News</a></div>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-12">
-                <div class="custom-pagination text-center">
-                  <span>1</span>
-                  <a href="#">2</a>
-                  <a href="#">3</a>
-                  <span class="more-page">...</span>
-                  <a href="#">10</a>
-                </div>
               </div>
             </div>
           </div> 
-
+          @endforeach
           <div class="col-md-3 ml-auto">
             <div class="mb-5">
               <h3 class="h5 text-black mb-3">Search</h3>
