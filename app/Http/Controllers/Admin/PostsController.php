befa2b4bc42c336a1f57f4fc1c35e952da7973d3
Murodjon <murodbek.0338@gmail.com>
2020-03-31 18:54:44 +0500

    create PostController

---
+++ app/Http/Controllers/Admin/PostsController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts=Post::all();
+
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request -> validate([
+
+            'title'=> 'required',
+            'short'=> 'required',
+            'content'=> 'required',
+            'img' => 'required|file|mimes:jpeg,jpg,png'
+        ]);
+        //Upload image to storage
+        $request->file('img')->store('posts', ['disk' => 'public']);
+        
+        Post::create($request->post());
+
+        return redirect()->route('admin.posts.index')->with('success', 'Item created!');
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post = Post::findOrFail($id);
+
+        return view('admin.posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $post = Post::findOrFail($id);
+
+        return view('admin.posts.edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+      $post=Post::findOrFail($id);
+
+      $request -> validate([
+        'title'=> 'required',
+        'short'=> 'required',
+        'content'=> 'required|min:50'
+      ]);
+
+        $post->update([
+            'title'=>$request->post('title'),
+            'short'=>$request->post('short'),
+            'content'=>$request->post('content')
+        ]);
+
+        return redirect()->route('admin.posts.index')->with('success','Item update!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $model = Post::findOrFail($id);
+
+        $model -> delete();
+
+        return redirect()->route('admin.posts.index')->with('delete', 'Item delete!');
+    }
+}
